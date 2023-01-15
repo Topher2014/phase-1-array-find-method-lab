9ad43369bf8f109f0cc78fe9db80e4a33a1fb175
topher ludlow <topherludlow@protonmail.com>
2023-01-14 20:02:06 -0800

    commit tree 57ac974f2a1de5115eddaa7df29402899b39eb12

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+// console.log(record.find(superbowlWin))
+ record.find(superbowlWin)
+
+function superbowlWin(record){
+    // console.log(record.result === 'W')
+    // console.log(record.year)
+    if (record.result === 'W'){
+        const variable = record.year
+        // console.log(variable)
+        return variable
+    }
+}
